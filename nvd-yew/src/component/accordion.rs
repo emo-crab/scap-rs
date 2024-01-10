1e15abb69009e85f53d0d343f966d63794ee01a3
cn-kali-team <kali-team@qq.com>
2024-01-10 10:01:33 +0800

    修复手风琴展开

--- nvd-yew/src/component/accordion.rs
+++ nvd-yew/src/component/accordion.rs
@@ -20,21 +20,19 @@ impl Component for Accordion {
   fn view(&self, ctx: &Context<Self>) -> Html {
     let AccordionProp { name, children, .. } = ctx.props();
     html! {
-      <div>
-      <div class="accordion" id={format!("accordion-{}",name.clone())} role="tablist" aria-multiselectable="true">
+      <div class="accordion" id={format!("accordion-{}",name.to_lowercase())} role="tablist" aria-multiselectable="true">
         <div class="accordion-item">
-          <h2 class="accordion-header" role="tab">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={format!("#collapse-{}",name.clone())} aria-expanded="true">
+          <h2 id={format!("heading-{}",name.to_lowercase())} class="accordion-header" role="tab">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={format!("#collapse-{}",name.to_lowercase())} aria-expanded="true">
               {name.clone()}
             </button>
           </h2>
-          <div id={format!("accordion-{}",name)} class="accordion-collapse collapse show" data-bs-parent={format!("#collapse-{}",name.clone())} style="">
+          <div id={format!("collapse-{}",name.to_lowercase())} class="accordion-collapse collapse show" data-bs-parent={format!("#accordion-{}",name.to_lowercase())} style="">
             <div class="accordion-body pt-0">
             {children.clone()}
             </div>
           </div>
         </div>
-      </div>
       </div>
     }
   }
