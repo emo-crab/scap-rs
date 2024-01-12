d894f04b31beccb0d63ae19b5117cccd042a6356
cn-kali-team <kali-team@qq.com>
2024-01-12 09:46:46 +0800

    支持spa

--- nvd-server/src/main.rs
+++ nvd-server/src/main.rs
@@ -1,5 +1,7 @@
 use actix_cors::Cors;
+use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::{http, middleware, web, App, HttpServer};
+
 use nvd_server::api::api_route;
 use nvd_server::init_db_pool;
 
@@ -20,11 +22,19 @@ async fn main() -> std::io::Result<()> {
       .service(web::scope("/api").configure(api_route))
       .service(
         actix_files::Files::new("/", "dist")
-          .show_files_listing()
-          .index_file("index.html"),
+          .index_file("index.html")
+          .default_handler(index),
       )
   })
   .bind(("0.0.0.0", 8888))?
   .run()
   .await
+}
+
+// 支持单页面应用，强制重定向到首页文件
+async fn index(reqs: ServiceRequest) -> actix_web::Result<ServiceResponse> {
+  let (req, _) = reqs.into_parts();
+  let file = actix_files::NamedFile::open_async("dist/index.html").await?;
+  let res = file.into_response(&req);
+  Ok(ServiceResponse::new(req, res))
 }
