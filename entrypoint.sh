de1136bc944a822c6b29b42054e316b5fea9f9b1
cn-kali-team <kali-team@qq.com>
2024-02-03 22:59:13 +0800

    cron helper

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Create directory for log.
 mkdir -p /app/var/log
-echo "* 2 * * * /prod/helper > /app/var/log/helper.log 2>&1"> /prod/crontab
+echo "* 2 * * * /prod/helper cve --api --hours 2 > /app/var/log/helper.log 2>&1"> /prod/crontab
 # Load cron configuration.
 crontab /prod/crontab
 # Start cron as a daemon.
