3a400b122f0990237452e5e8889996ebe5b99d50
cn-kali-team <kali-team@qq.com>
2024-02-03 22:17:29 +0800

    cron helper

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Terminate the script on first error.
+set -e
+
+# Create directory for log.
+mkdir -p /app/var/log
+echo "* 2 * * * /prod/helper > /app/var/log/helper.log 2>&1"> /prod/crontab
+# Load cron configuration.
+crontab /prod/crontab
+# Start cron as a daemon.
+cron
+
+# Run your main app.
+/prod/nvd-server
