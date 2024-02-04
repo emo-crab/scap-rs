e67a54b71f0ad05ec220841847801e29563b20a2
cn-kali-team <kali-team@qq.com>
2024-02-04 10:35:07 +0800

    sync env

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,14 @@ set -e
 
 # Create directory for log.
 mkdir -p /app/var/log
-echo "* 2 * * * /prod/helper cve --api --hours 2 > /app/var/log/helper.log 2>&1"> /prod/crontab
+echo "* 2 * * * /prod/helper cve --api --hours 2 > /app/var/log/helper.log 2>&1"> /etc/cron.d/crontab
 # Load cron configuration.
-crontab /prod/crontab
+crontab /etc/cron.d/crontab
 # Start cron as a daemon.
-cron
+{
+  echo "DATABASE_URL=${DATABASE_URL}"
+} >> /.env
+cron -f &
 
 # Run your main app.
 /prod/nvd-server
