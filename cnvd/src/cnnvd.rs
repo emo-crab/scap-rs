58c14560ada43b36281e3df85f8af9211aa247a3
cn-kali-team <kali-team@qq.com>
2024-04-02 15:00:37 +0800

    添加CNNVD接口，用于翻译漏洞详情

--- cnvd/src/cnnvd.rs
+++ cnvd/src/cnnvd.rs
@@ -1,11 +1,13 @@
 // CNVD的反爬需要执行js设置cookie，而且有防火墙。所以选CNNVD的API
 #![allow(clippy::large_enum_variant)]
 
-use crate::error::Error;
 use chrono::NaiveDate;
 use derive_builder::Builder;
 use reqwest::{ClientBuilder, RequestBuilder};
 use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+
 const BASE_URL: &str = "https://www.cnnvd.org.cn/web/";
 
 #[derive(Debug, Clone)]
@@ -13,6 +15,7 @@ pub struct CNNVDApi {
   base_path: String,
   client: reqwest::Client,
 }
+
 impl CNNVDApi {
   pub fn new() -> Result<Self, Error> {
     let api_client = ClientBuilder::new()
@@ -37,6 +40,7 @@ impl CNNVDApi {
     Ok(result)
   }
 }
+
 impl CNNVDApi {
   pub async fn detail(&self, query: DetailParameters) -> Result<CNNVDResult, Error> {
     let u = format!(
@@ -50,6 +54,7 @@ impl CNNVDApi {
     self.request(self.client.post(u).json(&query)).await
   }
 }
+
 #[derive(Default, Debug, Clone, Serialize, Deserialize, Builder)]
 #[serde(rename_all = "camelCase")]
 #[builder(setter(into), default)]
@@ -58,6 +63,7 @@ pub struct DetailParameters {
   pub vul_type: Option<String>,
   pub cnnvd_code: Option<String>,
 }
+
 #[derive(Default, Debug, Clone, Serialize, Deserialize, Builder)]
 #[serde(rename_all = "camelCase")]
 #[builder(setter(into), default)]
@@ -99,13 +105,14 @@ pub struct Detail {
   pub recevice_vul_detail: Option<String>,
 }
 
+// 漏洞详情
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CNNVDDetail {
   pub id: Option<String>,
   pub vul_name: String,
   pub cnnvd_code: String,
-  pub cve_code: String,
+  pub cve_code: Option<String>,
   pub publish_time: String,
   pub is_official: i64,
   pub vendor: Option<String>,
@@ -144,6 +151,7 @@ pub struct CNNVDDetail {
   pub varchar1: String,
 }
 
+// 漏洞列表
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct VulList {
@@ -153,6 +161,7 @@ pub struct VulList {
   pub page_size: i64,
 }
 
+// 单个记录
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Record {
@@ -168,6 +177,7 @@ pub struct Record {
   pub vul_type: String,
 }
 
+// 从列表的单个记录转为详情搜索参数
 impl From<Record> for DetailParameters {
   fn from(val: Record) -> Self {
     DetailParameters {
